b311a316e14432283a4e2feaa8e84a1fa752b544
Dario Anongba Varela <dario.anongba@gmail.com>
2025-12-10 15:01:47 +0100

    entities: add interactive transfer entities

--- entities/transfer.go
+++ entities/transfer.go
@@ -30,3 +30,43 @@ type Recipient struct {
 	// Amount is the amount of asset units to send.
 	Amount uint64
 }
+
+// InteractiveSendRequest represents a request to send assets interactively.
+type InteractiveSendRequest struct {
+	// AssetID is the 32-byte asset identifier.
+	AssetID [32]byte
+
+	// Amount is the number of asset units to send.
+	Amount uint64
+
+	// ReceiverKeys contains the keys derived by the receiver.
+	ReceiverKeys DerivedKeys
+}
+
+// SendResult represents the result of a completed send operation.
+type SendResult struct {
+	// TransferTxid is the anchor transaction ID (32 bytes, not reversed).
+	TransferTxid [32]byte
+
+	// AnchorTx is the raw anchor transaction bytes.
+	AnchorTx []byte
+
+	// Outputs contains details about each transfer output.
+	Outputs []TransferOutput
+}
+
+// TransferOutput represents a single output in a transfer.
+type TransferOutput struct {
+	// Outpoint is the output location in "txid:index" format.
+	Outpoint string
+
+	// ScriptKey is the 33-byte compressed public key locking this output.
+	ScriptKey [33]byte
+
+	// Amount is the number of asset units in this output.
+	Amount uint64
+
+	// ProofBlob is the transition proof for this output.
+	// For interactive sends, this must be delivered to the receiver.
+	ProofBlob []byte
+}
